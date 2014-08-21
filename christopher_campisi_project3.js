4c7a32e209e772ac94414afd1d19e726f85d91e9
Chris <chris.campisi@icloud.com>
2014-08-21 07:30:27 -0700

    Testing with JSON and most likely keeping changes for assignment requirement.

--- christopher_campisi_project3.js
+++ christopher_campisi_project3.js
@@ -1,1 +1,11 @@
-alert("JavaScript works!");
+var text = '{"name":"Christopher Campisi","class":"SDI: 1408","date":"Aug 21st 2014"}'
+
+var obj = JSON.parse(text);
+
+console.log(text);
+console.log(obj);
+
+document.getElementById("myInfo").innerHTML =
+obj.name + "<br>" +
+obj.class + "<br>" +
+obj.date;
