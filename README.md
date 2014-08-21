7a8afe61505f69d74ad320f26128dcca799711ab
Chris <chris.campisi@icloud.com>
2014-08-21 07:42:03 -0700

    Modified the readme file and fixed some formatting on the JSON input.

---
+++ README.md
@@ -0,0 +1,4 @@
+mdv
+======
+These documents are for Project 3 of my SDI class.
+I am experimenting with JSON.
--- christopher_campisi_project3.js
+++ christopher_campisi_project3.js
@@ -1,11 +1,16 @@
-var text = '{"name":"Christopher Campisi","class":"SDI: 1408","date":"Aug 21st 2014"}'
+var text = '{"name":"Christopher Campisi","class":"SDI: 1408","date":"Aug 21st 2014", "assignment":"Project 3"}'
 
 var obj = JSON.parse(text);
 
-console.log(text);
-console.log(obj);
-
+//displaying the JSON elements at the top of the HTML webpage and at the bottom for the last element.
 document.getElementById("myInfo").innerHTML =
-obj.name + "<br>" +
-obj.class + "<br>" +
-obj.date;
+	obj.name + "<br><br>" +
+	obj.class + "<br><br>" +
+	obj.date;
+
+document.getElementById("assign").innerHTML =
+	obj.assignment;
+	
+document.getElementById("completion").innerHTML =
+	obj.date + "<br>" +
+	obj.name;
